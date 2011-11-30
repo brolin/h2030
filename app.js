31eb7c84ed318f3ada4d963fd95c5af723a37172
brolin <brolin108@gmail.com>
2011-11-30 07:56:40 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,88 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var app = module.exports = express.createServer();
+var cluster = require('cluster');
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Horizontes 2030'
+  });
+});
+
+app.get('/EscenarioTerritorial', function(req, res){
+  res.render('EscenarioTerritorial', {
+    title: 'Escenario Territorial'
+  });
+});
+
+app.get('/Resultados', function(req, res){
+  res.render('Resultados', {
+    title: 'Resultados'
+  });
+});
+
+app.get('/Documentos', function(req, res){
+  res.render('Documentos', {
+    title: 'Documentos'
+  });
+});
+
+app.get('/Fotografias', function(req, res){
+  res.render('Fotografias', {
+    title: 'Fotografías'
+  });
+});
+
+app.get('/Mapas', function(req, res){
+  res.render('Mapas', {
+    title: 'Mapas'
+  });
+});
+
+app.get('/Contactenos', function(req, res){
+  res.render('Contactenos', {
+    title: 'Contáctenos'
+  });
+});
+
+// Only listen on $ node app.js
+
+cluster(app)
+    .use(cluster.logger('logs'))
+    .use(cluster.stats())
+    .use(cluster.pidfiles('pids'))
+    .use(cluster.cli())
+    .use(cluster.repl(8889))
+    .listen(9451);
+
+
+// if (!module.parent) {
+//   app.listen(9451);
+//   console.log("Express server listening on port %d", app.address().port);
+// }
